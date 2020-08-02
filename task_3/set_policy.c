95f2d0bb919e95c5922d385988fd7640b65ac5a1
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-02 16:31:17 +0300

    middle

--- task_3/set_policy.c
+++ task_3/set_policy.c
@@ -9,24 +9,29 @@
 #include <linux/sched.h>
 #include <sys/types.h>
 // https://man7.org/linux/man-pages/man2/sched_setattr.2.html
-struct sched_attr {
-    uint32_t size;
-
-    uint32_t sched_policy;
-    uint64_t sched_flags;
-
-    /* SCHED_NORMAL, SCHED_BATCH */
-    int32_t sched_nice;
-
-    /* SCHED_FIFO, SCHED_RR */
-    uint32_t sched_priority;
-
-    /* SCHED_DEADLINE (nsec) */
-    uint64_t sched_runtime;
-    uint64_t sched_deadline;
-    uint64_t sched_period;
-};
-
+// struct sched_attr {
+//     uint32_t size;
+//
+//     uint32_t sched_policy;
+//     uint64_t sched_flags;
+//
+//     /* SCHED_NORMAL, SCHED_BATCH */
+//     int32_t sched_nice;
+//
+//     /* SCHED_FIFO, SCHED_RR */
+//     uint32_t sched_priority;
+//
+//     /* SCHED_DEADLINE (nsec) */
+//     uint64_t sched_runtime;
+//     uint64_t sched_deadline;
+//     uint64_t sched_period;
+// };
+// SCHED_OTHER : 0
+// SCHED_FIFO : 1
+// SCHED_RR : 2
+// SCHED_BATCH : 4
+// SCHED_IDLE : 5
+// SCHED_DEADLINE : 6
  int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
  {
     return syscall(__NR_sched_setattr, pid, attr, flags);
@@ -36,29 +41,15 @@ struct sched_attr {
    // print process id
    pid_t pid = getpid();
    printf("pid: %d\n", pid);
-   printf("SCHED_OTHER: %d\n", SCHED_OTHER);
-   printf("SCHED_FIFO: %d\n", SCHED_FIFO);
-   printf("SCHED_RR: %d\n", SCHED_RR);
-   printf("SCHED_BATCH: %d\n", SCHED_BATCH);
-   printf("SCHED_IDLE: %d\n", SCHED_IDLE);
-   printf("SCHED_DEADLINE: %d\n", SCHED_DEADLINE);
 
-   // struct sched_attr attr = {
-   //      .size = sizeof(attr),
-   //      .sched_policy = atoi(argv[1]),
-   //      .sched_runtime = 30000000,
-   //      .sched_period = 100000000,
-   //      .sched_deadline = 100000000
-   //  };
      struct sched_attr attr = {
-         // .size = sizeof(attr),
          .sched_policy = atoi(argv[1]),
          .sched_priority = atoi(argv[2])
      };
 
-     pid_t tid = syscall(SYS_gettid);
+     // pid_t tid = syscall(SYS_gettid);
 
-     if (sched_setattr(tid, &attr, 0))
+     if (sched_setattr(pid, &attr, 0))
          perror("sched_setattr()");
      while(1){};
      return 0;
