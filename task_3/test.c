f4dd88749a53c15e98058b87db8f87d0f5cb602d
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-02 15:40:57 +0300

    fifo

--- task_3/test.c
+++ task_3/test.c
@@ -4,6 +4,7 @@
 int main(int argc,char* argv[])
 {
   struct sched_param sp;
-  sched_setscheduler(atoi(argv[1]), atoi(argv[2]), &sp);
+  // sched_setscheduler(atoi(argv[1]), atoi(argv[2]), &sp);
+  sched_setscheduler(atoi(argv[1]), SCHED_FIFO, &sp);
   return 0;
 }
