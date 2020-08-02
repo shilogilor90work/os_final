abb9c808e352dd1526f25f497352e476f2cd67d1
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-02 16:51:14 +0300

    includes

--- task_3/test.c
+++ task_3/test.c
@@ -1,13 +1,13 @@
 // https://stackoverflow.com/questions/50082317/is-sched-deadline-officially-supported-in-ubuntu-16-04
 
-#define _GNU_SOURCE
-#include <stdint.h>
+// #define _GNU_SOURCE
+// #include <stdint.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/syscall.h>
+// #include <unistd.h>
+// #include <sys/syscall.h>
 #include <sched.h>
-#include <linux/sched.h>
-#include <sys/types.h>
+// #include <linux/sched.h>
+// #include <sys/types.h>
 // https://man7.org/linux/man-pages/man2/sched_setattr.2.html
 
  int main(int argc, char* argv[]) {
