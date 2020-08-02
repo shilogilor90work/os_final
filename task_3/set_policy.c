94768add7eb07ea6841ac81b307f55b5f782ee97
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-02 16:06:34 +0300

    asfa

--- task_3/set_policy.c
+++ task_3/set_policy.c
@@ -1,46 +1,45 @@
-// #include <sys/types.h>
-#include <sched.h>
+// https://stackoverflow.com/questions/50082317/is-sched-deadline-officially-supported-in-ubuntu-16-04
+
+#define _GNU_SOURCE
+#include <stdint.h>
 #include <stdio.h>
-// #include <sys/time.h>
-// #include <sys/resource.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sched.h>
+#include <linux/sched.h>
+#include <sys/types.h>
+// https://man7.org/linux/man-pages/man2/sched_setattr.2.html
+struct sched_attr {
+               u32 size;              /* Size of this structure */
+               u32 sched_policy;      /* Policy (SCHED_*) */
+               u64 sched_flags;       /* Flags */
+               s32 sched_nice;        /* Nice value (SCHED_OTHER,
+                                         SCHED_BATCH) */
+               u32 sched_priority;    /* Static priority (SCHED_FIFO,
+                                         SCHED_RR) */
+               /* Remaining fields are for SCHED_DEADLINE */
+               u64 sched_runtime;
+               u64 sched_deadline;
+               u64 sched_period;
+           };
 
-int main(int argc,char* argv[])
-{
-  int status;
-  // if missing argument
+ int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
+ {
+    return syscall(__NR_sched_setattr, pid, attr, flags);
+ }
 
-    if(argc<3)
-        printf("please enter the process id and priorty...\n");
-    // if argument exists
-    if(argc>=3)
-    {
-      struct sched_param sp;
-      int policy = atoi(argv[1]);
-      int pid = atoi(argv[2]);
+ int main(int argc, char* argv[]) {
 
-      sched_setscheduler(policy, pid, &sp);
-      // switch (argv[1])
-      // ​{
-      //     case DEADLINE_SCHED:
-      //       // statements
-      //       break;
-      //
-      //     case SCHED_OTHER:
-      //       // statements
-      //       break;
-      //     case SCHED_RR:
-      //       // statements
-      //       break;
-      //     case SCHED_IDLE:
-      //       // statements
-      //       break;
-      //     case SCHED_FIFO:
-      //       // statements
-      //       break;
-      //     default:
-      //       // default statements
-      //       printf("not vaild priorty value\n");
-      // }
-    }
-    return 0;
-}
+     struct sched_attr attr = {
+         .size = sizeof(attr),
+         .sched_policy = atoi(argv[1]),
+         .sched_priority = atoi(argv[2])
+     };
+
+     pid_t tid = syscall(SYS_gettid);
+
+     if (sched_setattr(tid, &attr, 0))
+         perror("sched_setattr()");
+
+     return 0;
+ }
