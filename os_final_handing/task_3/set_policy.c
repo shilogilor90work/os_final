dcdbf4dfd4485ec2286ecee7b07c981cb56e8428
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-03 20:06:48 +0300

    ready

--- os_final_handing/task_3/set_policy.c
+++ os_final_handing/task_3/set_policy.c
@@ -1,18 +1,20 @@
-
+// imports
 #include <stdio.h>
 #include <sched.h>
  int main(int argc, char* argv[]) {
    // print process id
    pid_t pid = getpid();
    printf("pid: %d\n", pid);
-
-     const struct sched_param attr = {
-         .sched_priority = atoi(argv[2])
-     };
-
-     sched_setscheduler(0, atoi(argv[1]), &attr);
-     while(1){
-       sleep(1);
-     };
-     return 0;
+   // change priority on the struct
+   const struct sched_param attr = {
+       .sched_priority = atoi(argv[2])
+   };
+   // http://www.qnx.com/developers/docs/6.5.0/index.jsp?topic=%2Fcom.qnx.doc.neutrino_lib_ref%2Fs%2Fsched_setscheduler.html
+   // change sched policy
+   sched_setscheduler(0, atoi(argv[1]), &attr);
+   // keep thread alive
+   while(1){
+     sleep(1);
+   };
+   return 0;
  }
