d5f73f3876ee24d0ac1e7248feb0f47e39b17460
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-03 20:50:02 +0300

    struct

--- os_final_handing/task_3/set_policy.c
+++ os_final_handing/task_3/set_policy.c
@@ -1,6 +1,23 @@
 // imports
 #include <stdio.h>
 #include <sched.h>
+struct sched_attr {
+    uint32_t size;
+
+    uint32_t sched_policy;
+    uint64_t sched_flags;
+
+    /* SCHED_NORMAL, SCHED_BATCH */
+    int32_t sched_nice;
+
+    /* SCHED_FIFO, SCHED_RR */
+    uint32_t sched_priority;
+
+    /* SCHED_DEADLINE (nsec) */
+    uint64_t sched_runtime;
+    uint64_t sched_deadline;
+    uint64_t sched_period;
+};
  int main(int argc, char* argv[]) {
    // print process id
    pid_t pid = getpid();
