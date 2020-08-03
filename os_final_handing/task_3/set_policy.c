657bc9a09a67005def5d3ce8d3b166e06b4fa720
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-03 20:48:53 +0300

    sched dead

--- os_final_handing/task_3/set_policy.c
+++ os_final_handing/task_3/set_policy.c
@@ -7,12 +7,21 @@
    printf("pid: %d\n", pid);
    // change priority on the struct
    const struct sched_param attr = {
-       .sched_priority = atoi(argv[2]),
-       .sched_policy = atoi(argv[1])
+       .sched_priority = atoi(argv[2])
    };
    // http://www.qnx.com/developers/docs/6.5.0/index.jsp?topic=%2Fcom.qnx.doc.neutrino_lib_ref%2Fs%2Fsched_setscheduler.html
    // change sched policy
-   sched_setscheduler(0, atoi(argv[1]), &attr);
+   if (atoi(argv[1]) != 6)
+   {
+     sched_setscheduler(0, atoi(argv[1]), &attr);
+   } else
+   {
+     struct sched_attr sched_attribute = {
+         .sched_policy = atoi(argv[1]),
+         .sched_priority = atoi(argv[2])
+     };
+     sched_setattr(0, sched_attribute, 0);
+   }
    // keep thread alive
    while(1){
      sleep(1);
