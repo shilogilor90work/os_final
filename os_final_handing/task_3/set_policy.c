fcaf374c0178e3171290d8a933fa0cd689cd022e
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-03 21:02:17 +0300

    combined

--- os_final_handing/task_3/set_policy.c
+++ os_final_handing/task_3/set_policy.c
@@ -24,6 +24,10 @@ struct sched_attr {
     uint64_t sched_deadline;
     uint64_t sched_period;
 };
+int sched_setattr(pid_t pid, const struct sched_attr *attr, unsigned int flags)
+{
+   return syscall(__NR_sched_setattr, pid, attr, flags);
+}
  int main(int argc, char* argv[]) {
    // print process id
    pid_t pid = getpid();
@@ -43,7 +47,7 @@ struct sched_attr {
          .sched_policy = atoi(argv[1]),
          .sched_priority = atoi(argv[2])
      };
-     sched_setattr(0, sched_attribute, 0);
+     sched_setattr(0, &sched_attribute, 0);
    }
    // keep thread alive
    while(1){
