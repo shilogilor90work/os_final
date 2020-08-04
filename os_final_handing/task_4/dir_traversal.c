921ddef2a496fccce3189bd41eb3c505135a013e
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-04 13:15:49 +0300

    other tasks

--- os_final_handing/task_4/dir_traversal.c
+++ os_final_handing/task_4/dir_traversal.c
@@ -14,40 +14,33 @@ struct stat sb2;
 static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
 
+    // https://stackoverflow.com/questions/3984948/how-to-figure-out-if-a-file-is-a-link
+    // https://linux.die.net/man/2/stat
+    // get also inode data and sl info
+    lstat(fpath, &sb2);
     // because in task was asked no printing soft link.
-    // if (!(tflag == FTW_SL || tflag == FTW_SLN))
-    // {
-      // https://linux.die.net/man/2/stat
-      // get also inode data
-      lstat(fpath, &sb2);
-      if (!S_ISLNK(sb2.st_mode)) 
-            // perror("stat");
-      //       exit(EXIT_FAILURE);
-      //   }
-      // if (!S_ISLNK(sb2.st_mode))
-      {
-        printf("%-3s I-node: %-7ld %-10s FOR MORE INFO:  %2d %7jd   %-20s %-3d \n",
-            (tflag == FTW_D) ?   "D"   : (tflag == FTW_DNR) ? "DNR" :
-            (tflag == FTW_DP) ?  "DP"  : (tflag == FTW_F) ?   "F" :
-            (tflag == FTW_NS) ?  "NS"  : (tflag == FTW_SL) ?  "SL" :
-            (tflag == FTW_SLN) ? "SLN" : "???", (long) sb2.st_ino, fpath + ftwbuf->base, // for task
-            ftwbuf->level, (intmax_t) sb->st_size, fpath, ftwbuf->base); // for understanding
-      }
-    // }
+    if (!S_ISLNK(sb2.st_mode))
+    {
+      printf("%-3s I-node: %-7ld %-10s FOR MORE INFO:  %2d %7jd   %-20s %-3d \n",
+          (tflag == FTW_D) ?   "D"   : (tflag == FTW_DNR) ? "DNR" :
+          (tflag == FTW_DP) ?  "DP"  : (tflag == FTW_F) ?   "F" :
+          (tflag == FTW_NS) ?  "NS"  : (tflag == FTW_SL) ?  "SL" :
+          (tflag == FTW_SLN) ? "SLN" : "???", (long) sb2.st_ino, fpath + ftwbuf->base, // for task
+          ftwbuf->level, (intmax_t) sb->st_size, fpath, ftwbuf->base); // for understanding
+    }
     return 0;           /* To tell nftw() to continue */
 }
 
 int main(int argc, char *argv[])
 {
   int flags = 0;
-
+  // get dir to view
   if (argc > 2 && strchr(argv[2], 'd') != NULL)
        flags |= FTW_DEPTH;
    if (argc > 2 && strchr(argv[2], 'p') != NULL)
        flags |= FTW_PHYS;
 
-  if (nftw((argc < 2) ? "." : argv[1], display_info, 20, flags)
-           == -1) {
+  if (nftw((argc < 2) ? "." : argv[1], display_info, 20, flags) == -1) {
        perror("nftw");
        exit(EXIT_FAILURE);
    }
