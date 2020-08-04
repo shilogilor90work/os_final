060db31c6ad04f24c0fb5111cd10f873e9a5ff97
nJ3GxyfQ9sYbE-pzXy_e <shilo@xfind.ai>
2020-08-04 10:53:48 +0300

    file

--- os_final_handing/task_4/dir_traversal.c
+++ os_final_handing/task_4/dir_traversal.c
@@ -13,15 +13,16 @@ struct stat sb2;
 // https://linux.die.net/man/3/nftw
 static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
-  // https://linux.die.net/man/2/stat
-  // get also inode data
-  if (stat(fpath, &sb2) == -1) {
-        perror("stat");
-        exit(EXIT_FAILURE);
-    }
+
     // because in task was asked no printing soft link.
     if (!(tflag == FTW_SL || tflag == FTW_SLN))
     {
+      // https://linux.die.net/man/2/stat
+      // get also inode data
+      if (stat(fpath, &sb2) == -1) {
+            perror("stat");
+            exit(EXIT_FAILURE);
+        }
       printf("%-3s I-node: %-7ld %-10s FOR MORE INFO:  %2d %7jd   %-20s %-3d \n",
           (tflag == FTW_D) ?   "D"   : (tflag == FTW_DNR) ? "DNR" :
           (tflag == FTW_DP) ?  "DP"  : (tflag == FTW_F) ?   "F" :
@@ -32,8 +33,7 @@ static int display_info(const char *fpath, const struct stat *sb, int tflag, str
     return 0;           /* To tell nftw() to continue */
 }
 
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
    if (nftw(".", display_info, 20, 0) == -1) {
         perror("nftw");
